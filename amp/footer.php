20cad68e46763f56ee5d6a46e80f632a4c9d96f9
shu <shu@komish.jp>
2017-04-28 13:35:00 +0900

    footer comment change.

--- amp/footer.php
+++ amp/footer.php
@@ -2,7 +2,7 @@
 	<div>
 		<h2><?php echo esc_html( $this->get( 'blog_name' ) ); ?></h2>
 		<p>
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'amp' ) ); ?>"><?php printf( __( 'Powered by %s', 'amp' ), 'WordPress' ); ?></a>
+			<?php bloginfo( 'description' ); ?>
 		</p>
 		<a href="#top" class="back-to-top"><?php _e( 'Back to top', 'amp' ); ?></a>
 	</div>
