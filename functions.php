195082f7b77022810abe0561557b9b04bb73f7d6
shu <shu@komish.jp>
2017-05-02 14:06:37 +0900

    change author status

--- functions.php
+++ functions.php
@@ -253,7 +253,7 @@ function twentytwelve_entry_meta() {
 	// Translators: used between list items, there is a space after the comma.
 	$tag_list = get_the_tag_list( '', __( ', ', 'twentytwelve' ) );
  
- 	$author = sprintf( '<span class="author vcard"><i class="fa fa-pencil" aria-hidden="true"></i>&nbsp;<span class="post-author">%1$s</span>&nbsp;&nbsp;&nbsp;</span>',
+ 	$author = sprintf( '<span class="by-author"><i class="fa fa-pencil" aria-hidden="true"></i>&nbsp;<span class="author vcard"><span class="fn" rel="author">%1$s</span>&nbsp;&nbsp;&nbsp;</span></span>',
 		 get_the_author()
 	 );
 	 
