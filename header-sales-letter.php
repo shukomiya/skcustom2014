014eef332a959688d1bfbde0a9b8e3f71ab5e3e5
shu <shu@komish.jp>
2017-05-07 15:19:55 +0900

    fix delete breadclumb on sales-letter

--- header-sales-letter.php
+++ header-sales-letter.php
@@ -49,14 +49,6 @@
 				</ul>
 			</aside>
 		</hgroup>
-
-		<div class="breadcrumbs">
-		    <?php 
-		    	if (function_exists('bcn_display')) {
-		        	bcn_display();
-		    	}
-		    ?>
-		</div>
 	</header><!-- #masthead -->
 
 	<div id="main" class="wrapper">
