3ad8c7974d1fa228b7e087e4f47839ee0cba0123
shu <shu@komish.jp>
2017-06-19 18:34:20 +0900

    support amp-ad script moved.

--- amp/single.php
+++ amp/single.php
@@ -3,8 +3,8 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
-	<script async custom-element="amp-ad" src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"></script>
 	<?php do_action( 'amp_post_template_head', $this ); ?>
+	<script async custom-element="amp-ad" src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"></script>
 	<style amp-custom>
 		<?php $this->load_parts( array( 'style' ) ); ?>
 		<?php do_action( 'amp_post_template_css', $this ); ?>
