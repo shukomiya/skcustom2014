f79c016499f7fbb0c85da9966201760b876597a2
shu <shu@komish.jp>
2017-07-07 16:49:40 +0900

    add native adsense unit.

---
+++ ad/adsense/mg_in_feed.php
@@ -0,0 +1,11 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout="image-middle"
+     data-ad-layout-key="-6y+2b-fv+co+q0"
+     data-ad-client="ca-pub-7935009294964527"
+     data-ad-slot="8098197285"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
