9d86b09f7195a309018c42755ee9864f7d193785
shu <shu@komish.jp>
2017-07-08 17:13:25 +0900

    fix adsense side bottom

---
+++ ad/adsense/mg_sp_side_bottom.php
@@ -0,0 +1,13 @@
+<div>
+<div style="text-align: center; font-size: 87%;">PR</div>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- mg_sp_side_bottom -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7935009294964527"
+     data-ad-slot="5164764889"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div>
