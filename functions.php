cc6054031477fa2ad831a366b23518dd4ecf6545
shu <shu@komish.jp>
2017-09-15 19:35:23 +0900

    del jquery to header.

--- functions.php
+++ functions.php
@@ -743,17 +743,4 @@ function sk_get_product_list($atts, $content = null) {
 }
 add_shortcode('products', 'sk_get_product_list');
 
-if(!is_admin()){
-	function queue_cdn_jquery() {
-	wp_deregister_script('jquery');
-	wp_deregister_script('jquery-core');
-	wp_deregister_script('jquery-migrate');
-
-	wp_register_script('jquery', false, array('jquery-core', 'jquery-migrate'), '1.12.4', true);
-	wp_enqueue_script('jquery');
-	wp_enqueue_script('jquery-core', '//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js', array(), '1.12.4', true);
-	wp_enqueue_script('jquery-migrate', '//cdnjs.cloudflare.com/ajax/libs/jquery-migrate/1.4.1/jquery-migrate.min.js', array(), '1.4.1', true);
-	}
-	add_action( 'wp_enqueue_scripts', 'queue_cdn_jquery' );
-}
 ?>
