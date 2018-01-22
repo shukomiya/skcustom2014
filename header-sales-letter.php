73fd8a25bebefa60fcef6eef6ea2d344a3968428
shu <shu@komish.jp>
2018-01-22 14:36:52 +0900

    add post custom @header-sales-letter.php

--- header-sales-letter.php
+++ header-sales-letter.php
@@ -19,6 +19,9 @@
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
+<?php if(post_custom('head_custom')): ?>
+<?php echo post_custom('head_custom'); ?>
+<?php endif; ?>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
 <title><?php wp_title( '|', true, 'right' ); ?></title>
