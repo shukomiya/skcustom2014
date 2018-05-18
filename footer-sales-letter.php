4e5d7e42d438cc7eda58863473e1f78138c7bf18
shu <shu@komish.jp>
2018-05-18 11:55:24 +0900

    edit footer url

--- footer-sales-letter.php
+++ footer-sales-letter.php
@@ -13,13 +13,13 @@
 	<footer id="colophon" role="contentinfo">
 		<aside class="footmenu">
 			<ul class="footmenu">
-				<li><a href="/privacy" target="_blank">プライバシーについて</a>&nbsp;&nbsp;|</li>
-				<li><a href="/profile">プロフィール</a>&nbsp;&nbsp;|</li>
-				<li><a href="/contact">問い合わせ</a></li>
+				<li><a href="<?php echo esc_url( home_url() ); ?>/privacy" target="_blank">プライバシーについて</a>&nbsp;&nbsp;|</li>
+				<li><a href="<?php echo esc_url( home_url() ); ?>/profile">プロフィール</a>&nbsp;&nbsp;|</li>
+				<li><a href="<?php echo esc_url( home_url() ); ?>/contact">問い合わせ</a></li>
 			</ul>
 		</aside>
 		<div class="site-info">
-			Copyright &copy; 2008-<?php echo date('Y'); ?> <a href="<?php bloginfo('wpurl') ?>"><?php bloginfo('name'); ?></a> All rights reserverd.
+			Copyright &copy; 2008-<?php echo date('Y'); ?> <a href="<?php echo esc_url( home_url('/') ); ?>"><?php bloginfo('name'); ?></a> All rights reserverd.
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
