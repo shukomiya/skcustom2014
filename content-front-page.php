71aa387741d7bea2427094838d51c7ca4eccdd33
shu <shu@komish.jp>
2018-06-10 16:22:43 +0900

    modify front-page.php add file content-front-page.php

---
+++ content-front-page.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header class="entry-header">
+			<?php entry_date(); ?>
+			<div class="entry-meta">
+			<?php twentytwelve_entry_meta(); ?>
+			<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<i class="fa fa-pencil-square-o" aria-hidden="true"></i>&nbsp;<span class="edit-link">', '</span>' ); ?></div>
+		</header>
+
+		<div class="entry-content">
+			<?php if ( ! is_page_template( 'page-templates/front-page.php' ) ) : ?>
+			<?php the_post_thumbnail(); ?>
+			<?php endif; ?>
+			<?php the_content(); ?>
+			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
+		</div><!-- .entry-content -->
+	</article><!-- #post -->
--- page-templates/front-page.php
+++ page-templates/front-page.php
@@ -17,14 +17,17 @@ get_header(); ?>
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php
+			while ( have_posts() ) :
+				the_post();
+?>
 				<?php if ( has_post_thumbnail() ) : ?>
 					<div class="entry-page-image">
 						<?php the_post_thumbnail(); ?>
 					</div><!-- .entry-page-image -->
 				<?php endif; ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
+				<?php get_template_part( 'content', 'front-page' ); ?>
 
 			<?php endwhile; // end of the loop. ?>
 
