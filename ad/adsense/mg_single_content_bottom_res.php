ae6611fff9221ee7ef56031ae1d0379693a55a5d
shu <shu@komish.jp>
2018-08-20 18:09:04 +0900

    add content bottom add file

---
+++ ad/adsense/mg_single_content_bottom_res.php
@@ -0,0 +1,14 @@
+<div style="margin: 47px 0;">
+<div style="font-size: 87%;">PR</div>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- mg_single_content_bottom_rs -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7935009294964527"
+     data-ad-slot="6643926882"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div>
