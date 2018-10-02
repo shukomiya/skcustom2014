839744d7f3c472d941c69128aec6ce24094df1dc
shu <shu@komish.jp>
2018-10-02 16:11:40 +0900

    new add value commerece

--- ad/mg_single_content_top.php
+++ ad/mg_single_content_top.php
@@ -1,4 +1,4 @@
 <div style="margin: 12px 0; padding: 12px 0;border-top: 1px silver solid;border-bottom: 1px silver solid; text-align: center;">
 <div style="font-size: 87%;">PR</div>
-<script language="javascript" src="//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=2567249&pid=885380772"></script><noscript><a href="//ck.jp.ap.valuecommerce.com/servlet/referral?sid=2567249&pid=885380772" target="_blank" rel="nofollow"><img src="//ad.jp.ap.valuecommerce.com/servlet/gifbanner?sid=2567249&pid=885380772" border="0"></a></noscript>
+<script language="javascript" src="//ad.jp.ap.valuecommerce.com/servlet/jsbanner?sid=2567249&pid=885401743"></script><noscript><a href="//ck.jp.ap.valuecommerce.com/servlet/referral?sid=2567249&pid=885401743" target="_blank" rel="nofollow"><img src="//ad.jp.ap.valuecommerce.com/servlet/gifbanner?sid=2567249&pid=885401743" border="0"></a></noscript>
 </div>
