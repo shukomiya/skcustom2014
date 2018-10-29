a5edcc74f9806178b4c0e77047afd8460cae0b1c
shu <shu@komish.jp>
2018-10-29 21:39:52 +0900

    add header image

--- header.php
+++ header.php
@@ -49,6 +49,9 @@
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 			<?php get_search_form() ?>
 		</hgroup>
+		<?php if ( get_header_image() ) : ?>
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a>
+		<?php endif; ?>
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></button>
 			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
