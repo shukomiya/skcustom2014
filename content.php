0ab92298bf3098c8549c78c50a8c1928139780fd
shu <shu@komish.jp>
2018-12-08 19:06:38 +0900

    mod adsense position.

--- content.php
+++ content.php
@@ -73,14 +73,18 @@
 					echo '<h2 class="add-section-title">関連記事</h2>';
 					related_posts(); 
 				}
+
+				/*
 				if ( $g_ad_enabled ) {
 					if ( !is_mobile() ) {
 						sk_get_the_ad('rakuten', 'content_bottom_336x280');
 					} else {
 						sk_get_the_ad('rakuten', 'content_bottom_300x160');
 					}
+					echo '<h2 class="add-section-title">関連記事</h2>';
 					sk_get_the_ad('adsense', 'mg_single_content_bottom_rel_ad');
 				}
+				*/
 			}
 			?>
 		</div><!-- .entry-content -->
