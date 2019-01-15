8da4ac4162de4dd7c2520bb1093e60ccf860dee4
shu <shu@komish.jp>
2019-01-15 12:57:32 +0900

    uses rakuten motion widget

--- content.php
+++ content.php
@@ -67,15 +67,13 @@
 					echo '<h2 class="add-section-title">関連記事</h2>';
 					sk_get_the_ad('adsense', 'mg_single_content_bottom_rel_ad');
 				}
-				/*
-				if ( is_ad_enabled() ) {
+				if ( $g_ad_enabled && is_no_adsense() ) {
 					if ( !is_mobile() ) {
 						sk_get_the_ad('rakuten', 'content_bottom_336x280');
 					} else {
 						sk_get_the_ad('rakuten', 'content_bottom_300x160');
 					}
 				}
-				*/
 			}
 			?>
 		</div><!-- .entry-content -->
