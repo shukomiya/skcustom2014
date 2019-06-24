ddcdf58d676ab715a316b65c86570eed2d26cb75
shu <shu@komish.jp>
2019-06-25 08:21:37 +0900

    mod rakuten-ad

--- ad/rakuten/content_bottom_motion_travel.php
+++ ad/rakuten/content_bottom_motion_travel.php
@@ -1,4 +1,3 @@
-<div style="margin: 48px 0 24px 0;text-align:center">
-<div style="font-size: 87%;">PR</div>
+<div style="margin: 48px 0;text-align:center">
 <script type="text/javascript">rakuten_design="slide";rakuten_affiliateId="0ad3f435.7e98cbcf.0ad3f436.178dafe2";rakuten_items="tra-ranking";rakuten_genreId="tra-allzenkoku";rakuten_size="468x160";rakuten_target="_blank";rakuten_theme="gray";rakuten_border="on";rakuten_auto_mode="on";rakuten_genre_title="off";rakuten_recommend="on";rakuten_ts="1560493275304";</script><script type="text/javascript" src="https://xml.affiliate.rakuten.co.jp/widget/js/rakuten_widget_travel.js"></script>
 </div>
